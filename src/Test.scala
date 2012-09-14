c0c31d348bc3280e6cfc800f1437473ab1567b46
Alexey <alexey.kardapoltsev@gmail.com>
2012-09-14 10:44:50 +0600

    User could define any currency in money file

--- src/Test.scala
+++ src/Test.scala
@@ -1,3 +1,4 @@
+import io.Source
 import ru.numerals.Numerals._
 import ru.numerals.Money._
 import ru.numerals.Fractions._
@@ -11,8 +12,8 @@ import ru.numerals.Fractions._
 object Test {
   def main (args: Array[String]) {
 
-    1 to 100 foreach (x => println (num2Str (BigInt (x))))
-    10000 to 10100 foreach (x => println (ruMoney (BigInt (x))))
-    1 to 100 foreach (x => println (fraction2Str ("%s/%s".format (x - 1, x))))
+    println(ruMoney(BigInt(10013)))
+    println(money(BigInt(10013),"USD"))
+
   }
 }
--- src/ru/numerals/Money.scala
+++ src/ru/numerals/Money.scala
@@ -1,6 +1,9 @@
 package ru.numerals
 
 import ru.numerals.Numerals._
+import io.Source
+import java.io.{FileNotFoundException, BufferedWriter, FileWriter, File}
+import tools.nsc.io.Path
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,37 +12,111 @@ import ru.numerals.Numerals._
  */
 
 object Money {
-
   import Gender._
 
-  private val rubles = List ("рубль", "рубля", "рублей")
-  private val kopecks = List ("копейка", "копейки", "копеек")
+
+  /**
+   * Convert x to money using default currency RUR from money file
+   * @param x amount to be converted
+   * @return converted string
+   */
 
 
-  def ruMoney (x: BigInt) = convertRuMoney (x)
+  def ruMoney (x: BigInt) = convertMoney (x,"RUR")
 
 
-  private def convertRuMoney (x: BigInt) = {
+  /**
+   * Converting x to money using currency cur
+   * @param x amount to be converted
+   * @param cur currency. Should be present in money file
+   * @return converted string
+   */
+
+
+  def money (x : BigInt,cur : String) = convertMoney(x,cur)
+
+
+  private def convertMoney (x: BigInt, currency : String) = {
+
+    val cur = getCurrency(currency)
+
     val r = x / 100
     val k = x % 100
     val rubNum = num2Str (r, Masculine)
     val kopNum = num2Str (k, Feminine)
     val rub = (r % 100).toInt match {
-      case a if (10 < a && 20 > a) => rubles (2)
+      case a if (10 < a && 20 > a) => cur (3)
       case a => a % 10 match {
-        case 1 => rubles (0)
-        case y if Set (2, 3, 4) contains y => rubles (1)
-        case _ => rubles (2)
+        case 1 => cur (1)
+        case y if Set (2, 3, 4) contains y => cur (2)
+        case _ => cur (3)
       }
     }
     val kop = (k % 100).toInt match {
-      case a if (10 < a && 20 > a) => kopecks (2)
+      case a if (10 < a && 20 > a) => cur (6)
       case a => a % 10 match {
-        case 1 => kopecks (0)
-        case y if Set (2, 3, 4) contains y => kopecks (1)
-        case _ => kopecks (2)
+        case 1 => cur (4)
+        case y if Set (2, 3, 4) contains y => cur (5)
+        case _ => cur (6)
       }
     }
     "%s %s %s %s" format(rubNum, rub, kopNum, kop)
+  }
+
+
+  private def getCurrency (cur : String) = {
+    val currency = getResources.filter(cur == _.head)
+
+    if (currency.isEmpty)
+      throw new RuntimeException ("No such currency - %s!" format cur)
+    if (1 != currency.length)
+      throw new RuntimeException ("Currency %s defined %d times in money file!".
+        format (cur,currency.length))
+
+    currency.head
+  }
+
+
+  private def getResources = {
+   val r = try {
+     Source.fromFile("money.txt").getLines().toList
+      .filter(!_.startsWith("//")).map(_.split(":").toList)
+    } catch {
+      case e : FileNotFoundException => {
+        createDefaultMoneyFile()
+        Source.fromFile("money.txt").getLines().toList
+          .filter(!_.startsWith("//")).map(_.split(":").toList)
+      }
+    }
+
+    if (r.isEmpty)
+      throw new RuntimeException ("No currency in money file!")
+
+    val wrongLines = r.filter(7 != _.length)
+    if (!wrongLines.isEmpty)
+      throw new RuntimeException (
+        "Wrong money file! Chech lines %s".format (wrongLines))
+
+    r
+  }
+
+
+  private def createDefaultMoneyFile () {
+    try{
+      val fileStream = new FileWriter("money.txt")
+
+      fileStream.write(
+        "// Lines started with // will be ignored\n" +
+        "//\n" +
+        "// валюта:один:два:много:один:два:много\n" +
+        "//\nRUR:рубль:рубля:рублей:копейка:копейки:копеек\n" +
+        "USD:доллар:доллара:долларов:цент:цента:центов")
+
+      fileStream.close()
+
+    }catch  {
+      //TODO: Exception handling
+      case e : Exception => System.err.println("Error: " + e.getMessage)
+    }
   }
 }
